d931752bbb23b3dffed90823790bf66548e73452
chadwithuhc <chadwithuhc@gmail.com>
2020-07-20 21:44:49 -0600

    Netlify CMS attempt

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,7 +2,7 @@ import 'src/styles/app.sass'
 import datastore from '../data/store'
 
 // preload all data
-datastore.fetch()
+// datastore.fetch()
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
