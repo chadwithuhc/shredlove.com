d16e31b57a2beb79a605ea2a6391a74194355fa6
chadwithuhc <chadwithuhc@gmail.com>
2020-10-06 22:08:16 -0600

    Custom Widget for Credits

---
+++ public/admin/custom-widgets/credits.js
@@ -0,0 +1,139 @@
+(function(){
+
+  const CreditsControl = createClass({
+
+    // this.props.value
+    // [
+    //   { type: 'skateboarder', person: 'don' }
+    // ]
+    // handleChange: function(e) {
+    //   const separator = this.props.field.get('separator', ', ')
+    //   this.props.onChange(e.target.value.split(separator).map((e) => e.trim()));
+    // },
+
+    setHasFocus: function (e) {
+      this.setState({
+        focusedField: {
+          index: Number(e.target.dataset.fieldIndex),
+          prop: e.target.dataset.targetProp
+        }
+      })
+    },
+
+    getInitialState: function() {
+      return {
+        focusedField: {
+          // index: 0
+          // prop: 'type'
+        }
+      }
+    },
+
+    handleChangeForIndex: function(index) {
+      return (e) => {
+        const creditValues = this.props.value || [{}];
+        if (index > creditValues.length - 1) { // create a new item if needed
+          creditValues.push({})
+        }
+
+        // console.log('e.target.dataset', e.target.dataset, 'e.target.value', e.target.value, index)
+
+        const updatedCreditValues = creditValues.map((creditValue, i) => {
+          if (i === index) {
+            // console.log('e.target.dataset', e.target.dataset)
+            return {
+              ...creditValue,
+              [e.target.dataset.targetProp]: e.target.value
+            }
+          }
+
+          return creditValue
+        })
+        
+        // console.log('creditValues', creditValues)
+        // console.log('updatedCreditValues', updatedCreditValues)
+
+        this.props.onChange(updatedCreditValues)
+      }
+    },
+
+    renderInputRow: function(creditValue = null, valueIndex = 0) {
+      const { focusedField } = this.state
+      // console.log('focusedField', focusedField)
+
+      return [
+        h('input', {
+          id: !!creditValue ? this.props.forID : undefined,
+          'data-field-index': valueIndex,
+          type: 'text',
+          className: 'credits-input',
+          value: creditValue ? creditValue.type : '',
+          'data-target-prop': 'type',
+          placeholder: 'skateboarder',
+          onChange: this.handleChangeForIndex(valueIndex),
+          onFocus: this.setHasFocus,
+          autoFocus: focusedField.index === valueIndex && focusedField.prop === 'type'
+        }),
+        h('input', {
+          'data-field-index': valueIndex,
+          type: 'text',
+          className: 'credits-input',
+          value: creditValue ? creditValue.person : '',
+          'data-target-prop': 'person',
+          placeholder: 'don',
+          onChange: this.handleChangeForIndex(valueIndex),
+          onFocus: this.setHasFocus,
+          autoFocus: focusedField.index === valueIndex && focusedField.prop === 'person'
+        })
+      ]
+    },
+  
+    render: function() {
+      // const separator = this.props.field.get('separator', ', ');
+      const creditValues = this.props.value || [{}];
+
+      return [
+        h('div', {
+          className: this.props.classNameWrapper,
+          children: [
+            h('div', { className: 'credits-grid-inputs' }, [
+              h('label', {}, 'Credit Type'),
+              h('label', {}, 'Credit Person'),
+              ...creditValues.map((creditValue, i) => this.renderInputRow(creditValue, i)),
+              ...this.renderInputRow(null, creditValues.length)
+            ])
+          ]
+        }),
+      ]
+    },
+  });
+  
+  const creditsOptions = [
+    { key: 'skateboarder', value: 'Skateboarder' },
+    { key: 'director', value: 'Director' }
+  ]
+  
+  const CreditsPreview = createClass({
+    render: function() {
+      const credits = this.props.value;
+
+      return h('dl', {},
+        credits.map((credit, index) => {
+          return [
+            h('dt', {key: index}, credit.type),
+            h('dd', {key: index}, credit.person)
+          ];
+        })
+      );
+    }
+  });
+  
+  const schema = {
+    properties: {
+      separator: { type: 'string' },
+    },
+  }
+
+  CMS.registerWidget('credits', CreditsControl, CreditsPreview, schema);
+
+})()
