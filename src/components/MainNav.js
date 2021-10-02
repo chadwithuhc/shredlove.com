36d8b694433fd9028b9597164b358a8c8d32ca46
cheddar <73146636+ched-dev@users.noreply.github.com>
2021-10-01 21:07:37 -0600

    Mobile nav redesign

--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -10,20 +10,22 @@ export default function MainNav() {
           <span className="archives"><small>The</small> Archives</span>
         </a>
       </Link>
-      <ul className="link-list">
-        <li><a href="/crew">Crew</a></li>
-        <li><Link href="/timeline"><a>Timeline</a></Link></li>
-        {Object.values(MediaTypes).map(mediaType => (
-          <li key={mediaType.label}><Link href={mediaType.url}><a>{mediaType.labelPlural}</a></Link></li>
-        ))}
-      </ul>
-
-      <div className="social-links">
-        <span className="social-links-title">FOLLOW<br/>@shredlove</span>
-        <ul className="social-links-icons">
-          <li><a href="https://twitter.com/shredlove" target="_blank" rel="noopener nofollow"><i className="fa fa-twitter" /></a></li>
-          <li><a href="https://instagram.com/shredlove" target="_blank" rel="noopener nofollow"><i className="fa fa-instagram" /></a></li>
+      <div className="nav-groups">
+        <ul className="link-list">
+          <li><a href="/crew">Crew</a></li>
+          <li><Link href="/timeline"><a>Timeline</a></Link></li>
+          {Object.values(MediaTypes).map(mediaType => (
+            <li key={mediaType.label}><Link href={mediaType.url}><a>{mediaType.labelPlural}</a></Link></li>
+          ))}
         </ul>
+
+        <div className="social-links">
+          <span className="social-links-title">@shredlove</span>
+          <ul className="social-links-icons">
+            <li><a href="https://twitter.com/shredlove" target="_blank" rel="noopener nofollow"><i className="fa fa-twitter" /></a></li>
+            <li><a href="https://instagram.com/shredlove" target="_blank" rel="noopener nofollow"><i className="fa fa-instagram" /></a></li>
+          </ul>
+        </div>
       </div>
     </nav>
   )
